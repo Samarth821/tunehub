fe3782f2186c7dbe19277a57c0dd2b542ddf55b1
Samarth <Samarth@DESKTOP-7TP9PEF>
2024-02-28 21:52:05 +0530

    1st version of tune hub

---
+++ src/main/java/com/example/demo/controller/SongController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entity.Song;
+import com.example.demo.service.SongService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class SongController {
+	@Autowired
+	SongService service;
+	@PostMapping("/addSong")
+	public String addSong(@ModelAttribute Song song) {
+		boolean songStatus = service.songExists(song.getName());
+		if(songStatus==false)
+		{
+			service.addSong(song);
+			System.out.println("Song added");
+		}
+		else
+		{
+			System.out.println("Song exists");
+		}
+		
+		return "adminHome.html";
+	}
+	@GetMapping("/viewSongs")
+	public String viewSongs(Model model) {
+		List<Song> songsList =  service.fetchAllSongs();
+		model.addAttribute("songs", songsList);
+		return "displaySongs.html";
+	}
+	@GetMapping("/playSongs")
+	public String playSongs(Model model) {
+		boolean premiumUser = false;
+		if(premiumUser)
+		{
+			List<Song> songsList =  service.fetchAllSongs();
+			model.addAttribute("songs", songsList);
+			return "displaySongs.html";
+		}
+		return "makePayment.html";	
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,83 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Song;
+import com.example.demo.entity.Users;
+import com.example.demo.service.SongService;
+import com.example.demo.service.UsersService;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class UsersController {
+	@Autowired
+	UsersService service;
+	
+	@Autowired
+	SongService songService;
+
+	@PostMapping("/registration")
+	public String addUsers(@ModelAttribute Users user) 
+	{
+		boolean userStatus = service.emailExists(user.getEmail());
+		if (userStatus  == false) {
+			service.addUser(user);
+			System.out.println("User added");
+		} else {
+			System.out.println("User exists");
+		}
+		return "login.html";
+	}
+	@PostMapping("/validate")
+	public String validate(@RequestParam("email") String email, 
+			@RequestParam("password") String password, HttpSession session, Model model) 
+	{
+		if(service.validateUser(email, password))
+		{
+			String role = service.getRole(email);
+			
+			session.setAttribute("email", email);
+			if(role.equals("admin"))
+			{
+				return "adminHome.html";
+			}
+			else {
+                Users user = service.getUser(email);
+                boolean userStatus = user.isPremium();
+                List<Song> songsList =  songService.fetchAllSongs();
+
+                model.addAttribute("isPremium", userStatus);
+                model.addAttribute("songs", songsList);
+                return "customerHome.html";
+			}
+		}
+		else
+		{
+			return "login.html";
+		}
+		
+	}
+	
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session)  {
+		session.invalidate();
+		return "login.html";
+	}
+	
+	
+	
+	
+
+}
